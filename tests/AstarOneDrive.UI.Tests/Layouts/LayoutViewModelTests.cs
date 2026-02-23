959ecf75fbcea74b5c5ec9c579335b5693fee2ca
Jason Barden <jason.barden1@outlook.com>
2026-02-23 11:49:57 +0000

    test(ui): add failing layout viewmodel phase 7 tests (RED)

---
+++ tests/AstarOneDrive.UI.Tests/Layouts/LayoutViewModelTests.cs
@@ -0,0 +1,75 @@
+using AstarOneDrive.UI.Home;
+using AstarOneDrive.UI.Layouts;
+using Shouldly;
+
+namespace AstarOneDrive.UI.Tests.ViewModels.Layouts;
+
+public sealed class LayoutViewModelTests
+{
+    [Fact]
+    public void ExplorerLayoutViewModel_CanInstantiateWithMainWindowContext()
+    {
+        var mainWindowViewModel = new MainWindowViewModel();
+
+        var sut = new ExplorerLayoutViewModel(mainWindowViewModel);
+
+        sut.MainWindow.ShouldBeSameAs(mainWindowViewModel);
+        sut.MainWindow.Accounts.ShouldBeSameAs(mainWindowViewModel.Accounts);
+    }
+
+    [Fact]
+    public void DashboardLayoutViewModel_CanInstantiateWithMainWindowContext()
+    {
+        var mainWindowViewModel = new MainWindowViewModel();
+
+        var sut = new DashboardLayoutViewModel(mainWindowViewModel);
+
+        sut.MainWindow.ShouldBeSameAs(mainWindowViewModel);
+        sut.MainWindow.Settings.ShouldBeSameAs(mainWindowViewModel.Settings);
+    }
+
+    [Fact]
+    public void TerminalLayoutViewModel_CanInstantiateWithMainWindowContext()
+    {
+        var mainWindowViewModel = new MainWindowViewModel();
+
+        var sut = new TerminalLayoutViewModel(mainWindowViewModel);
+
+        sut.MainWindow.ShouldBeSameAs(mainWindowViewModel);
+        sut.MainWindow.Sync.ShouldBeSameAs(mainWindowViewModel.Sync);
+    }
+
+    [Fact]
+    public void ExplorerLayoutCommand_Execute_UpdatesSummary()
+    {
+        var mainWindowViewModel = new MainWindowViewModel();
+        var sut = new ExplorerLayoutViewModel(mainWindowViewModel);
+
+        sut.RefreshSummaryCommand.Execute(null);
+
+        sut.SyncSummary.ShouldBe(mainWindowViewModel.Sync.Status);
+    }
+
+    [Fact]
+    public void DashboardLayoutCommand_Execute_ChangesThemeSelection()
+    {
+        var mainWindowViewModel = new MainWindowViewModel();
+        var sut = new DashboardLayoutViewModel(mainWindowViewModel);
+        var initialTheme = sut.CurrentTheme;
+
+        sut.CycleThemeCommand.Execute(null);
+
+        sut.CurrentTheme.ShouldNotBe(initialTheme);
+    }
+
+    [Fact]
+    public void TerminalLayoutCommand_Execute_UpdatesTerminalStatus()
+    {
+        var mainWindowViewModel = new MainWindowViewModel();
+        var sut = new TerminalLayoutViewModel(mainWindowViewModel);
+
+        sut.RunHealthCheckCommand.Execute(null);
+
+        sut.TerminalStatus.ShouldBe("Connected");
+    }
+}
